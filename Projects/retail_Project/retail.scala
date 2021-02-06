f94935bf5cc6cdb95d9eb4d3d58ffe90f2a4aa13
sd2beatles <53164959+sd2beatles@users.noreply.github.com>
2021-02-07 07:14:39 +0900

    project_updated

--- Projects/retail_Project/retail.scala
+++ Projects/retail_Project/retail.scala
@@ -1,119 +1,158 @@
-
 package retail.db
-import scala.collection.mutable._
-import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
-
-import org.apache.spark.sql._
-import org.apache.log4j._
+import org.apache.spark.sql.types.{DateType, DoubleType, IntegerType, StringType, StructType}
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, Dataset, Encoder}
 import org.apache.spark.sql.functions._
 import com.typesafe.config.{Config, ConfigFactory}
-import org.apache.spark.sql.{DataFrame, Dataset}
-import org.apache.spark.sql.types.{DoubleType, FloatType, IntegerType, StringType, StructType}
-import org.apache.spark.sql.functions.regexp_extract
+import org.apache.spark.ml.feature.{StringIndexer,OneHotEncoder,VectorAssembler}
+import org.apache.spark.ml.Pipeline
+import java.sql.Date
 
-import scala.util.matching.Regex
 
-trait BaseSetHandler[T]{
+trait BaseSetHandler[T,S]{
   val subPath:String
-  def readContents(spark:SparkSession)(implicit enc:Encoder[T]):Dataset[T]
+  val fileName:String
+  val schema:StructType
+  def readContent(spark:SparkSession)(implicit enc:Encoder[T]):Dataset[S]
 }
 
 object Default{
-  val default_enprov=ConfigFactory.load()
+  val default_env=ConfigFactory.load()
   val default_header="dev"
-  val assignedHeader=default_enprov.getConfig(default_header)
+  val assignedHeader=default_env.getConfig(default_header)
+  val default_partition_no:Int=5
 }
 
 
-case class Order(order_id:Int,order_date:String,order_customer_id:Int,order_status:String)
-object OrderInfo extends BaseSetHandler[Order]{
-  override val subPath:String="input.base.dir"
-  val fileAdd=Default.assignedHeader.getString(subPath)+"/orders.txt"
+case class Order(orderID:Int,date:String,customerID:Int,orderStatus:String)
+case class OrderHistory(orderID:Int,orderDate:Date,day_of_week:String,orderStatus:String)
+object OrderRecord extends BaseSetHandler[Order,OrderHistory]{
+  override val subPath:String="input.dir"
+  override val fileName:String="orders.txt"
+  val filePath=Default.assignedHeader.getString(subPath)+fileName
+  override val schema=new StructType()
+    .add("orderID",IntegerType)
+    .add("date",StringType)
+    .add("customerID",IntegerType)
+    .add("orderStatus",StringType)
 
-  val schema=new StructType()
-    .add("order_id",IntegerType)
-    .add("order_date",StringType)
-    .add("order_customer_id",IntegerType)
-    .add("order_status",StringType)
 
-  override def readContents(spark:SparkSession)(implicit enc:Encoder[Order]):Dataset[Order]={
+
+  override def readContent(spark:SparkSession)(implicit enc:Encoder[Order]):Dataset[OrderHistory]={
     import spark.implicits._
-    val table=spark
+    val df=spark
       .read
-      .option("sep",",")
+      .options(Map("sep"->",","header"->"true"))
       .schema(schema)
-      .csv(fileAdd)
+      .csv(filePath)
       .as[Order]
 
-    //do a trim operation in the colum of order date
-    val pattern="\\d{4}-\\d{2}-\\d{2}"
-    val result=table.withColumn("order_date",regexp_extract(col("order_date"),pattern,0)).as[Order]
-    result
+    val modified=timeManagement(spark,df)
+    modified
+  }
 
+  def timeManagement(spark:SparkSession,data:Dataset[Order])={
+    import spark.implicits._
+    val modified=data
+      .withColumn("orderDate",to_date(col("date"),"yyyy-MM-dd"))
+      .withColumn("day_of_week",date_format(col("orderDate"),"EEEE"))
+      .select("orderID","orderDate","day_of_week","customerID","orderStatus")
+      .as[OrderHistory]
+    modified
   }
 }
 
-case class ProductItem(id:Int,order_id:Int,product_id:Int,quantity:Int,subtotal:Float,product_price:Float)
-object OrderItem extends BaseSetHandler[ProductItem]{
-  override val subPath:String="input.base.dir"
-  val fileAdd=Default.default_enprov.getConfig(Default.default_header).getString(subPath)+"/order_items.txt"
-  val schema=new StructType()
+case class OrderInfo(id:Int,orderID:Int,productID:Int,quantity:Int,purchaseValue:Double,unitPrice:Double)
+object OrderItem extends BaseSetHandler[OrderInfo,OrderInfo]{
+  override val subPath:String="input.dir"
+  override val fileName:String=Default.assignedHeader.getString(subPath)+"/order_items.txt"
+  override val schema:StructType=new StructType()
     .add("id",IntegerType)
-    .add("order_id",IntegerType)
-    .add("product_id",IntegerType)
+    .add("orderID",IntegerType)
+    .add("productID",IntegerType)
     .add("quantity",IntegerType)
-    .add("subtotal",FloatType)
-    .add("product_price",FloatType)
+    .add("purchaseValue",DoubleType)
+    .add("unitPrice",DoubleType)
 
-  override def readContents(spark: SparkSession)(implicit enc: Encoder[ProductItem]): Dataset[ProductItem] = {
-    val df=spark
-      .read
-      .schema(schema)
-      .option("sep",",")
-      .csv(fileAdd)
-      .as[ProductItem]
-
+  override def readContent(spark:SparkSession)(implicit enc:Encoder[OrderInfo]):Dataset[OrderInfo]={
+  import spark.implicits._
+   val df=spark
+    .read
+    .option("sep",",")
+     .schema(schema)
+    .csv(fileName)
+    .as[OrderInfo]
     df
   }
 }
 
 
-object Merge {
-  val outputBaseDir = Default.assignedHeader.getString("output.base.dir")
 
-  def mergeTable(df1: Dataset[Order], df2: Dataset[ProductItem]) = {
-    val result = df1.where("order_status in ('CLOSED','Complete')")
-      .join(df2, usingColumns = Seq("order_id"), joinType = "inner")
-      .groupBy("order_date", "product_id")
-      .agg(round(sum("subtotal"), 2).alias("revenue"))
-      .orderBy(col("order_date"), col("revenue").desc)
-    result
+object Modeling{
+  def mergeTable(df1:Dataset[OrderHistory],df2:Dataset[OrderInfo],partitionNo:Int=5)={
 
+    val merged=df1.where("orderStatus in ('CLOSED','Complete')")
+      .join(df2,usingColumns=Seq("orderID"),joinType="inner")
+      .coalesce(partitionNo)
+    merged
   }
 
+
+  def transformData(data:DataFrame,selectCols:Array[String],category:String): Unit ={
+    val features=handlingCategorical(selectCols,category)
+    val pipeline=new Pipeline().setStages(features)
+    val fittedPipeline=pipeline.fit(data)
+    val transformed=fittedPipeline.transform(data)
+
+    transformed
+  }
+
+  def handlingCategorical(selectCols:Array[String],category:String)={
+    val indexCol=category+"_index"
+    val encodedCol=category+"_encoded"
+    val totalCols=selectCols:+encodedCol
+
+    val indexer=new StringIndexer()
+      .setInputCol(category)
+      .setOutputCol(indexCol)
+
+    val encoder=new OneHotEncoder()
+      .setInputCol(category+"_index")
+      .setOutputCol(encodedCol)
+
+    val vectorAssembler=new VectorAssembler()
+      .setInputCols(totalCols)
+      .setOutputCol("features")
+
+    Array(indexer,encoder,vectorAssembler)
+  }
+  
 }
 
-object test2 {
-  def main(args: Array[String]) = {
-    Logger.getLogger("org").setLevel(Level.ERROR)
-    @transient lazy val logger = Logger.getLogger(getClass.getName)
-    logger.info("Retrive an excution mode.By default,it is an internal device")
-    val exMode = Default.default_enprov.getConfig(Default.default_header).getString("execution.mode")
 
-    val spark = SparkSession
+
+object test3{
+  def main(args:Array[String]): Unit ={
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+
+    val basicMode=Default.assignedHeader.getString("execution.mode")
+    val spark=SparkSession
       .builder
-      .appName("revenueAnalysis")
-      .master(exMode)
+      .appName("reatialAnalysis")
+      .master(basicMode)
       .getOrCreate()
 
+    spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
     import spark.implicits._
-    System.out.println("Now,we are porcessing an assigned task")
-    val orderDF = OrderInfo.readContents(spark)
-    val itemDF = OrderItem.readContents(spark)
-    val data=Merge.mergeTable(orderDF,itemDF)
-    System.out.println("Print out the first three rows of the merged table")
-    data.show(3)
-    data.write.mode("overwrite").json(Default.assignedHeader.getString("output.base.dir"))
+    val orderData=OrderRecord.readContent(spark)
+    val info=OrderItem.readContent(spark)
+    val merged=Modeling.mergeTable(orderData,info)
+    merged.show(3)
+
   }
 }
+
+
+
